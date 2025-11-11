265f40f052e378f6c8a0d3bd9e6b449ea4308f6e
Batman <lucas.parente0808@gmail.com>
2025-11-10 23:01:24 -0300

    Novas funcionalidades

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -6,6 +6,7 @@ export interface AuthRequest extends Request {
   user?: any;
 }
 
+
 export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
